063e6c3d460ac39614f0a5046f835036605d1f0a
palakbansal94 <palakbansal94@gmail.com>
2015-11-29 14:54:07 +0530

    fblogin connected with our server

--- poo/app/scripts/controllers/loading.js
+++ poo/app/scripts/controllers/loading.js
@@ -1,33 +1,7 @@
 'use strict';
 
-angular.module('pooApp').controller('userlCtrl', function($scope, $location, $http, localStorageService){
-$scope.click1=function(){
-	$scope.url=baseurl+"authenticate";
-	$scope.data= {
-			"email_id": $scope.text3,
-			"password": $scope.text4
-		};
-	
-	$http.post($scope.url,$scope.data).then(function successCallback(response){
-		    console.log(response.status);
-			$scope.status=response.status;
-			window.localStorage['authtoken'] = response.data.authtoken;
-			window.localStorage['email_id'] = $scope.text3;
-			console.log(window.localStorage['email_id']);
-			console.log(window.localStorage['authtoken']);
-			console.log(response.data.authtoken);
-			$location.path('/list');
-			//alert("You are successfully logged off");
-		},
-		function errorCallback(response){
-			console.log(response);
-			$scope.status=response.status;
-				$scope.response=response.data.error;
-		});
-	
-	};	
-
-$scope.fblogin=function(){
+angular.module('pooApp').controller('loadingCtrl', function($scope, $location, $http, localStorageService){
+	$scope.fblogin=function(){
 	$scope.url=baseurl+"fblogin";
 
 	$scope.data={
@@ -65,7 +39,5 @@ $scope.fblogin=function(){
 	});
 	}
 	
-	};	
-	
-//window.location='../../views/secondpg.html';
-});
+	};
+});	
